928f53088139ccf972441d49cab53299fb011aea
ManishSingh00 <165392944+ManishSingh00@users.noreply.github.com>
2025-05-11 16:32:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+console.log(3 + 3)
+console.log("Manish Singh")
+
+
+// data types
+// number  2 to power 53
+// bigint
+// string 
+// boolean
+// null
+// undefined 
+
+console.log(typeof null)  // output-- object
+console.log(typeof undefined)  // output -- undefined
