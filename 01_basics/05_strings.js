0be6a832b2b69eaae76d1bd71a7391c2e6ebbb56
ManishSingh00 <165392944+ManishSingh00@users.noreply.github.com>
2025-05-12 15:30:30 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,15 @@
+// string declaration methods:--
+
+// 1. Using double quotes
+let str1 = "Hello, world!";
+
+// 2. Using single quotes
+let str2 = 'Hello, world!';
+
+// 3. Using backticks (template literals)
+let name = "Manish Singh";
+let str3 = `Hello, ${name}!`;  // Allows interpolation and multi-line strings
+
+console.log(name);
+
+// string methods in JS (read article)
