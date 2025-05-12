cd64935634f8b4bcd40b892fead686bd2dde834d
ManishSingh00 <165392944+ManishSingh00@users.noreply.github.com>
2025-05-12 14:52:18 +0000

    data types and memory

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -11,3 +11,29 @@ const id = Symbol("123");
 const newId = Symbol("123")
 
 console.log(id === newId) // output == False   // both id  and newId have different value or  symbol
+
+
+// ********************************************************************************
+// Stack and Heap Memory in js:-
+
+// stack(primitive data type)   nd  heap(non-primitive data type)
+
+let my_name = "manish"
+let surname = my_name
+surname = "singh"
+
+console.log(my_name)
+console.log(surname)
+
+let userone = {
+    age : 20,
+    gender : "male"
+}
+let userTwo = userone
+userTwo.age=25
+
+console.log(userone.age); // 25 values get updated due to heap memory in case of object(non-primitive)
+console.log(userTwo.age);
+
+
+
