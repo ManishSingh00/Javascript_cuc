d6c9ad7d991bfef5057a1c8c963b618d9245db91
ManishSingh00 <165392944+ManishSingh00@users.noreply.github.com>
2025-05-16 17:06:46 +0000

    control flow

---
+++ 04_control_flow/if_else.js
@@ -0,0 +1,75 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
+
+
+// some special OPERATORS:--
+// Nullish Coalescing Operator (??): to handle the case of  null , undefined
+//Only triggers on: null or undefined
+//Does NOT trigger on: false, 0, NaN, '' (empty string)
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20   // ouput is 10
+
+
+
+console.log(val1);
+
+// Ternary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
