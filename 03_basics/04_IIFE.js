44de6ec3d02f4ac8b10367392001187d928019f6
ManishSingh00 <165392944+ManishSingh00@users.noreply.github.com>
2025-05-16 14:50:41 +0000

    iife

---
+++ 03_basics/04_IIFE.js
@@ -0,0 +1,16 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+// why IIFE?
+// 1) immediately invoked (i.e., run) — right after it's defined.
+// 2) IIFEs is used to create isolated scopes and avoid polluting the global namespace.
+// 3)If multiple scripts are loaded, variables inside an IIFE won't interfere with other scripts.
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();/// if you will not put semi colon(;) at the end of IIFEs then you will get error if you try to execute
+// some other functions after that IIFEs bcz IIFEs by its own did not understand where they have to end;
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('manish')  // here manish is passed as an argument to the function
