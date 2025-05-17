1f07bd41a36232a2b74f7247447f1cab2fb49f22
ManishSingh00 <165392944+ManishSingh00@users.noreply.github.com>
2025-05-17 11:49:28 +0000

    chaining

---
+++ 05_iterations.js/six.js
@@ -0,0 +1,10 @@
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+// console.log(newNums);
+// const newNums = myNumers                  // CHAINING method
+//                 .map((num) => num * 10 )
+//                 .filter( (num) => num >= )
+//                 .map( (num) => num + 1) // here value of num will not come from original array, it will come from output obtained in its previos step
+console.log(newNums);
