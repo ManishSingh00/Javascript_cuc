4efc7ba0dd67682e6afb7f91176ddb38cbe6cf12
ManishSingh00 <165392944+ManishSingh00@users.noreply.github.com>
2025-05-29 17:15:37 +0000

    prototypal inheritance

--- 09_advance_one/4_prototype.js
+++ 09_advance_one/4_prototype.js
@@ -17,3 +17,62 @@ let heroPower = {
     }
 }
 
+Object.prototype.manish = function(){ // this is present in all object
+    console.log(`manish is present in all objects`);
+}
+
+Array.prototype.heymanish = function(){
+    console.log(`manish says hello`);
+}
+
+// heroPower.manish()
+// myHeros.manish() // same output because arrays also inherit from Object in JS
+
+//Let’s visualize the prototype chain of both:
+// heroPower (a plain object): heroPower → Object.prototype → null
+// myHeros (an array): myHeros → Array.prototype → Object.prototype → null
+
+
+myHeros.heymanish()
+heroPower.heymanish()  // it will give error bcz heropower is an object
+
+
+
+// inheritance
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport  // If a property is not found in TASupport, JS will look in TeachingSupport (PROTOTYPAL INHERITANCE)
+}
+
+Teacher.__proto__ = User
+
+
+
+// MODERN SYNTAX:--
+Object.setPrototypeOf(TeachingSupport, Teacher) //"If a property is not found in TeachingSupport, go look in Teacher."
+
+let anotherUsername = "ChaiAurCode     "
+
+// answer of question arises in first paragraph:--
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"hitesh".trueLength()
+"iceTea".trueLength()
