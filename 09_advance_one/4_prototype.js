1077d6209ea9df4b40d531c6b9ac32f773b3fc36
ManishSingh00 <165392944+ManishSingh00@users.noreply.github.com>
2025-05-29 15:58:01 +0000

    protottype

---
+++ 09_advance_one/4_prototype.js
@@ -0,0 +1,19 @@
+// let myName = "Manish     "
+// let myLastName = "Singh     "
+
+// console.log(myName.trueLength);
+
+// I want to make this trueLength as a method which returns the actual 
+// length of strings always.....
+
+
+let myHeros = ["thor", "spiderman"]
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
