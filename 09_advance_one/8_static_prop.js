aa6566957cb69347742717d76b0774e2846d81df
ManishSingh00 <165392944+ManishSingh00@users.noreply.github.com>
2025-05-30 07:59:36 +0000

    static method

---
+++ 09_advance_one/8_static_prop.js
@@ -0,0 +1,27 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const manish = new User("MANISH")
+// console.log(manish.createId()) // INVALID because Static methods are called on the class 
+// itself, not on instances of the class.
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId()); // INVALID
